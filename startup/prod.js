388852a89b4a15d9f6c154d808a485a7be574116
Sep (Macbook Pro) <me@sepmotameni.com>
2021-03-28 18:15:59 -0400

    Added Cors

--- startup/prod.js
+++ startup/prod.js
@@ -1,11 +1,12 @@
+const express = require("express");
 const helmet = require("helmet");
 const compression = require("compression");
 const morgan = require("morgan");
-const express = require("express");
+const cors = require("cors");
 
 module.exports = function (app) {
   app.enable("trust proxy");
-  app.use("cors");
+  app.use(cors());
   app.use(helmet());
   app.use(compression());
   app.use(morgan("short"));
