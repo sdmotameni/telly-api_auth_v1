030c8d1715073b927bfc5522065c1fed495c3a87
Sep (Macbook Pro) <me@sepmotameni.com>
2021-03-28 18:35:51 -0400

    Added Labels for validation

--- utils/validation.js
+++ utils/validation.js
@@ -2,22 +2,22 @@ const Joi = require("@hapi/joi");
 
 module.exports.validateReg = function (user) {
   const schema = Joi.object({
-    profileId: Joi.string().max(100).required(),
-    name: Joi.string().min(1).max(30).required(),
-    email: Joi.string().min(1).max(30).required(),
-    password: Joi.string().min(1).max(25).required(),
-    phone: Joi.string().length(10).required(),
+    profileId: Joi.string().max(100).required().label("Profile ID"),
+    name: Joi.string().min(1).max(30).required().label("Name"),
+    email: Joi.string().min(1).max(30).required().label("Email"),
+    password: Joi.string().min(1).max(25).required().label("Password"),
+    phone: Joi.string().length(10).required().label("Phone Number"),
   });
   return schema.validate(user);
 };
 
 module.exports.validateUserUpdate = function (user) {
   const schema = Joi.object({
-    email: Joi.string().min(1).max(30),
-    name: Joi.string().min(1).max(30),
-    password: Joi.string().min(1).max(25),
-    phone: Joi.string().length(10),
-    bio: Joi.string().max(30),
+    email: Joi.string().min(1).max(30).label("Email"),
+    name: Joi.string().min(1).max(30).label("Name"),
+    password: Joi.string().min(1).max(25).label("Password"),
+    phone: Joi.string().length(10).label("Phone Number"),
+    bio: Joi.string().max(30).label("Bio"),
   });
   return schema.validate(user);
 };
@@ -29,8 +29,8 @@ module.exports.validateLinksUpdate = function (links) {
 
 module.exports.validateLoginReq = function (user) {
   const schema = Joi.object({
-    email: Joi.string().min(1).max(30).required(),
-    password: Joi.string().min(1).max(25).required(),
+    email: Joi.string().min(1).max(30).required().label("Email"),
+    password: Joi.string().min(1).max(25).required().label("Password"),
   });
   return schema.validate(user);
 };
