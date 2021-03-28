ee3d1d79c80f1961bd230a9b39a56b19a243e4c8
Sep (Macbook Pro) <me@sepmotameni.com>
2021-03-28 18:14:58 -0400

    new

--- startup/prod.js
+++ startup/prod.js
@@ -5,8 +5,7 @@ const express = require("express");
 
 module.exports = function (app) {
   app.enable("trust proxy");
-  // TODO: Setup Cors
-  //   app.use('cors');
+  app.use("cors");
   app.use(helmet());
   app.use(compression());
   app.use(morgan("short"));
