85c95383ebdbd99c644dbe90daacefb9b51faf1e
Sep (Macbook Pro) <me@sepmotameni.com>
2021-04-05 13:54:13 -0400

    Added sentry logger

--- middleware/asyncErrors.js
+++ middleware/asyncErrors.js
@@ -1,9 +1,8 @@
 // https://www.npmjs.com/package/express-async-errors
-
 const errorLogger = require("../utils/errorLogger");
 
 module.exports = function (err, _, res, _) {
   console.error(err.message, err);
-  errrorLogger.log(err);
+  errorLogger.log(err);
   res.status(500).send("500: Server error. Try again later.");
 };
