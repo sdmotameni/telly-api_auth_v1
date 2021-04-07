7a1b81c240f3e2389ec0a0e0a3bcca3f8c623941
Sep (Macbook Pro) <me@sepmotameni.com>
2021-04-07 09:08:10 -0400

    Email attribute will always be lowercase before processed

--- utils/trimObject.js
+++ utils/trimObject.js
@@ -2,6 +2,10 @@ module.exports.trimObject = function (obj) {
   const newObj = {};
 
   Object.keys(obj).forEach((ele) => {
+    if (ele == "email") {
+      obj[ele] = obj[ele].toLowerCase();
+    }
+
     var formattedEle = obj[ele].trim();
 
     if (formattedEle) {
